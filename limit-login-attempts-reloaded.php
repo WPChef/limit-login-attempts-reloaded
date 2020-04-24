c2125a61cda48145189b522d151e0266b107691f
Initiatus <initiatus@gmail.com>
2020-04-24 13:03:12 +0300

    Version 2.12.1

--- limit-login-attempts-reloaded.php
+++ limit-login-attempts-reloaded.php
@@ -5,7 +5,7 @@ Description: Limit the rate of login attempts, including by way of cookies and f
 Author: WPChef
 Author URI: https://wpchef.org
 Text Domain: limit-login-attempts-reloaded
-Version: 2.12.0
+Version: 2.12.1
 
 Copyright 2008 - 2012 Johan Eenfeldt, 2016 - 2020 WPChef
 */
