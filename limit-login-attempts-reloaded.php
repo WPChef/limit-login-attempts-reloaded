78c29456be9434a6994ac8969a50aa00c44b76e6
Guriy <initiatus@gmail.com>
2021-07-30 17:02:47 +0300

    2.23.1

--- limit-login-attempts-reloaded.php
+++ limit-login-attempts-reloaded.php
@@ -5,7 +5,7 @@ Description: Limit the rate of login attempts for each IP address.
 Author: Limit Login Attempts Reloaded
 Author URI: https://www.limitloginattempts.com/
 Text Domain: limit-login-attempts-reloaded
-Version: 2.23.0
+Version: 2.23.1
 
 Copyright 2008 - 2012 Johan Eenfeldt, 2016 - 2021 Limit Login Attempts Reloaded
 */
