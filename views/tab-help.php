5c4900fda9a5bc49d1ec3a1cbb5b745b62958ea6
Andrew <andriy.vasilchuk@gmail.com>
2022-12-22 13:59:18 +0200

    Add Help tab

---
+++ views/tab-help.php
@@ -0,0 +1,65 @@
+<?php
+
+if( !defined( 'ABSPATH' ) ) exit();
+
+/**
+ * @var $this Limit_Login_Attempts
+ */
+
+?>
+
+<div class="llar-help-page">
+    <div class="section-1">
+        <div class="block">
+            <div class="title"><?php _e( 'Upgrade Now To Access Premium Support', 'limit-login-attempts-reloaded' ); ?></div>
+            <div class="text"><?php _e( 'Our technical support team is available by email to help with any questions.', 'limit-login-attempts-reloaded' ); ?></div>
+            <div class="actions">
+                <a class="button button-primary" href="#" target="_blank">
+                    <?php _e( 'Upgrade To Premium', 'limit-login-attempts-reloaded' ); ?>
+					<span><?php _e( 'Starting from $8/month', 'limit-login-attempts-reloaded' ); ?></span>
+                </a>
+            </div>
+        </div>
+        <div class="block">
+            <div class="title"><?php _e( 'Free Support', 'limit-login-attempts-reloaded' ); ?></div>
+            <div class="text"><?php _e( 'Our technical support team is available by email to help with any questions.', 'limit-login-attempts-reloaded' ); ?></div>
+            <div class="actions">
+                <a class="button button-secondary" href="#" target="_blank"><?php _e( 'Go To Support Forums', 'limit-login-attempts-reloaded' ); ?></a>
+            </div>
+        </div>
+    </div>
+    <a href="#" target="_blank" class="gdpr-information-link"><?php _e( 'GDPR Information', 'limit-login-attempts-reloaded' ); ?></a>
+
+    <h2 class="all-doc-title"><a href="#" target="_blank"><?php _e( 'All Documentation', 'limit-login-attempts-reloaded' ); ?></a></h2>
+    <div class="documentation-section">
+        <div class="questions">
+            <a class="question" href="#" target="_blank">
+                <div class="title"><?php _e( 'Cloud Service & Security', 'limit-login-attempts-reloaded' ); ?></div>
+                <div class="desc"><?php _e( 'Questions regarding the cloud service including how to activate, logs and storage, and compliance.', 'limit-login-attempts-reloaded' ); ?></div>
+            </a>
+            <a class="question" href="#" target="_blank">
+                <div class="title"><?php _e( 'Technical Questions', 'limit-login-attempts-reloaded' ); ?></div>
+                <div class="desc"><?php _e( 'Questions regarding the cloud service including how to activate, logs and storage, and compliance.', 'limit-login-attempts-reloaded' ); ?></div>
+            </a>
+            <a class="question" href="#" target="_blank">
+                <div class="title"><?php _e( 'Accounts & Billing', 'limit-login-attempts-reloaded' ); ?></div>
+                <div class="desc"><?php _e( 'Questions regarding the cloud service including how to activate, logs and storage, and compliance.', 'limit-login-attempts-reloaded' ); ?></div>
+            </a>
+            <a class="question" href="#" target="_blank">
+                <div class="title"><?php _e( 'Pre-sales questions', 'limit-login-attempts-reloaded' ); ?></div>
+                <div class="desc"><?php _e( 'Questions regarding the cloud service including how to activate, logs and storage, and compliance.', 'limit-login-attempts-reloaded' ); ?></div>
+            </a>
+        </div>
+        <div class="top-list">
+            <div class="title"><?php _e( 'Top Topics and Questions', 'limit-login-attempts-reloaded' ); ?></div>
+            <ol>
+                <li><a href="#" target="_blank"><?php _e( 'Blocking Troubleshooting', 'limit-login-attempts-reloaded' ); ?></a></li>
+                <li><a href="#" target="_blank"><?php _e( 'Blocking Troubleshooting', 'limit-login-attempts-reloaded' ); ?></a></li>
+                <li><a href="#" target="_blank"><?php _e( 'Blocking Troubleshooting', 'limit-login-attempts-reloaded' ); ?></a></li>
+                <li><a href="#" target="_blank"><?php _e( 'Blocking Troubleshooting', 'limit-login-attempts-reloaded' ); ?></a></li>
+                <li><a href="#" target="_blank"><?php _e( 'Blocking Troubleshooting', 'limit-login-attempts-reloaded' ); ?></a></li>
+                <li><a href="#" target="_blank"><?php _e( 'Blocking Troubleshooting', 'limit-login-attempts-reloaded' ); ?></a></li>
+            </ol>
+        </div>
+    </div>
+</div>
