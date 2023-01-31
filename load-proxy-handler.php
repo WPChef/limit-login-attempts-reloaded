ed19aa83cbac5a9aae31313de5c7166854082b6f
Andrew <andriy.vasilchuk@gmail.com>
2023-01-31 15:25:51 +0200

    handle xmlrpc request

--- load-proxy-handler.php
+++ load-proxy-handler.php
@@ -2,9 +2,27 @@
 
 use LLAR\Core\Helpers;
 
-if( ( empty( $proxy_config ) ) || empty( $_POST['log'] ) ) return;
+if( empty( $proxy_config ) || !( $username = get_username_from_request() ) ) return;
 
-if( !( $username = trim( $_POST['log'] ) ) ) return;
+function get_username_from_request() {
+
+    if( $_SERVER['REQUEST_METHOD'] !== 'POST' ) return false;
+
+	if ( !empty( $_POST['log'] ) ) {
+		return trim( $_POST['log'] );
+	} else if( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST ) {
+
+		if( $data = @file_get_contents( 'php://input' ) ) {
+			$message = new IXR_Message( $data );
+
+			if ( !$message->parse() || $message->messageType != 'methodCall' ) return false;
+
+			if( !empty( $message->params[1] ) ) return $message->params[1];
+        }
+	}
+
+	return false;
+}
 
 require_once( 'lib/CidrCheck.php' );
 require_once( 'core/Helpers.php' );
@@ -93,12 +111,16 @@ class LoadProxyHandler {
 
 		curl_close($ch);
 
-		if( is_array( $response ) && $response['result'] === 'deny' ) {
+		if( is_array( $response ) && !empty( $response['result'] ) && $response['result'] === 'deny' ) {
 			$this->show_error_page();
         }
 	}
 
 	private function show_error_page() {
+	    if( defined( 'XMLRPC_REQUEST' ) && XMLRPC_REQUEST ) {
+		    header('HTTP/1.0 403 Forbidden');
+		    exit;
+        }
 		?>
 		<div class="llar-load-proxy-error-page">
 			<div class="inner-content">
@@ -139,5 +161,190 @@ class LoadProxyHandler {
         </style>
 		<?php
 		exit();
+	}
+}
+
+class IXR_Message
+{
+	var $message;
+	var $messageType;  // methodCall / methodResponse / fault
+	var $faultCode;
+	var $faultString;
+	var $methodName;
+	var $params;
+
+	// Current variable stacks
+	var $_arraystructs = array();   // The stack used to keep track of the current array/struct
+	var $_arraystructstypes = array(); // Stack keeping track of if things are structs or array
+	var $_currentStructName = array();  // A stack as well
+	var $_param;
+	var $_value;
+	var $_currentTag;
+	var $_currentTagContents;
+	// The XML parser
+	var $_parser;
+
+	function __construct($message)
+	{
+		$this->message = &$message;
+	}
+
+	function parse()
+	{
+		// first remove the XML declaration
+		// merged from WP #10698 - this method avoids the RAM usage of preg_replace on very large messages
+		$header = preg_replace( '/<\?xml.*?\?'.'>/s', '', substr( $this->message, 0, 100 ), 1 );
+		$this->message = trim( substr_replace( $this->message, $header, 0, 100 ) );
+		if ( '' == $this->message ) {
+			return false;
+		}
+
+		// Then remove the DOCTYPE
+		$header = preg_replace( '/^<!DOCTYPE[^>]*+>/i', '', substr( $this->message, 0, 200 ), 1 );
+		$this->message = trim( substr_replace( $this->message, $header, 0, 200 ) );
+		if ( '' == $this->message ) {
+			return false;
+		}
+
+		// Check that the root tag is valid
+		$root_tag = substr( $this->message, 0, strcspn( substr( $this->message, 0, 20 ), "> \t\r\n" ) );
+		if ( '<!DOCTYPE' === strtoupper( $root_tag ) ) {
+			return false;
+		}
+		if ( ! in_array( $root_tag, array( '<methodCall', '<methodResponse', '<fault' ) ) ) {
+			return false;
+		}
+
+		// Bail if there are too many elements to parse
+		$element_limit = 30000;
+		if ( $element_limit && 2 * $element_limit < substr_count( $this->message, '<' ) ) {
+			return false;
+		}
+
+		$this->_parser = xml_parser_create();
+		// Set XML parser to take the case of tags in to account
+		xml_parser_set_option($this->_parser, XML_OPTION_CASE_FOLDING, false);
+		// Set XML parser callback functions
+		xml_set_object($this->_parser, $this);
+		xml_set_element_handler($this->_parser, 'tag_open', 'tag_close');
+		xml_set_character_data_handler($this->_parser, 'cdata');
+		$chunk_size = 262144; // 256Kb, parse in chunks to avoid the RAM usage on very large messages
+		$final = false;
+		do {
+			if (strlen($this->message) <= $chunk_size) {
+				$final = true;
+			}
+			$part = substr($this->message, 0, $chunk_size);
+			$this->message = substr($this->message, $chunk_size);
+			if (!xml_parse($this->_parser, $part, $final)) {
+				return false;
+			}
+			if ($final) {
+				break;
+			}
+		} while (true);
+		xml_parser_free($this->_parser);
+
+		// Grab the error messages, if any
+		if ($this->messageType == 'fault') {
+			$this->faultCode = $this->params[0]['faultCode'];
+			$this->faultString = $this->params[0]['faultString'];
+		}
+		return true;
+	}
+
+	function tag_open($parser, $tag, $attr)
+	{
+		$this->_currentTagContents = '';
+		$this->currentTag = $tag;
+		switch($tag) {
+			case 'methodCall':
+			case 'methodResponse':
+			case 'fault':
+				$this->messageType = $tag;
+				break;
+			/* Deal with stacks of arrays and structs */
+			case 'data':    // data is to all intents and puposes more interesting than array
+				$this->_arraystructstypes[] = 'array';
+				$this->_arraystructs[] = array();
+				break;
+			case 'struct':
+				$this->_arraystructstypes[] = 'struct';
+				$this->_arraystructs[] = array();
+				break;
+		}
+	}
+
+	function cdata($parser, $cdata)
+	{
+		$this->_currentTagContents .= $cdata;
+	}
+
+	function tag_close($parser, $tag)
+	{
+		$valueFlag = false;
+		switch($tag) {
+			case 'int':
+			case 'i4':
+				$value = (int)trim($this->_currentTagContents);
+				$valueFlag = true;
+				break;
+			case 'double':
+				$value = (double)trim($this->_currentTagContents);
+				$valueFlag = true;
+				break;
+			case 'string':
+				$value = (string)trim($this->_currentTagContents);
+				$valueFlag = true;
+				break;
+			case 'value':
+				// "If no type is indicated, the type is string."
+				if (trim($this->_currentTagContents) != '') {
+					$value = (string)$this->_currentTagContents;
+					$valueFlag = true;
+				}
+				break;
+			case 'boolean':
+				$value = (boolean)trim($this->_currentTagContents);
+				$valueFlag = true;
+				break;
+			case 'base64':
+				$value = base64_decode($this->_currentTagContents);
+				$valueFlag = true;
+				break;
+			/* Deal with stacks of arrays and structs */
+			case 'data':
+			case 'struct':
+				$value = array_pop($this->_arraystructs);
+				array_pop($this->_arraystructstypes);
+				$valueFlag = true;
+				break;
+			case 'member':
+				array_pop($this->_currentStructName);
+				break;
+			case 'name':
+				$this->_currentStructName[] = trim($this->_currentTagContents);
+				break;
+			case 'methodName':
+				$this->methodName = trim($this->_currentTagContents);
+				break;
+		}
+
+		if ($valueFlag) {
+			if (count($this->_arraystructs) > 0) {
+				// Add value to struct or array
+				if ($this->_arraystructstypes[count($this->_arraystructstypes)-1] == 'struct') {
+					// Add to struct
+					$this->_arraystructs[count($this->_arraystructs)-1][$this->_currentStructName[count($this->_currentStructName)-1]] = $value;
+				} else {
+					// Add to array
+					$this->_arraystructs[count($this->_arraystructs)-1][] = $value;
+				}
+			} else {
+				// Just add as a parameter
+				$this->params[] = $value;
+			}
+		}
+		$this->_currentTagContents = '';
 	}
 }
