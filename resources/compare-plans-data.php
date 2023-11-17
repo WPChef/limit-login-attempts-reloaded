2e14fbbec94e2ea78f661b4cf2cbdcd652179f61
slavadvent <slavadvent@users.noreply.github.com>
2023-11-17 20:46:13 +0300

    Premium Page Layout section-3. Step-36

---
+++ resources/compare-plans-data.php
@@ -0,0 +1,202 @@
+<?php
+/**
+ * Array for plans comparison block
+ *
+ */
+
+$compare_list = [
+    'Recommended For' => [
+        'Free' => 'Basic Login Protection For Entry-Level WordPress Websites.',
+        'Premium' => 'Websites that are Self-Managed and Require Optimal Safeguards Against Present and Future Hazards.',
+        'Premium +' => 'Websites Under Elevated Threat that Require Additional Features & Resources To Operate At Their Optimal Performance.',
+        'Professional' => 'Websites Under Frequent Attack That Require The Most Comprehensive Protection To Protect Sensitive Data.',
+    ],
+    'Monthly Price' => [
+        'Free' => '-',
+        'Premium' => 'ssss',
+        'Premium +' => 'ssss',
+        'Professional' => 'ssss',
+    ],
+    'Annual Price' => [
+        'Free' => '-',
+        'Premium' => 'ssss',
+        'Premium +' => 'ssss',
+        'Professional' => 'ssss',
+    ],
+    'Lifetime Price' => [
+        'Free' => '-',
+        'Premium' => 'ssss',
+        'Premium +' => 'ssss',
+        'Professional' => 'ssss',
+    ],
+    'Limit Login Attempts' => [
+        'Free' => 'Yes',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Configurable Lockout Timings' => [
+        'Free' => 'Yes',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'GDPR Compliance' => [
+        'Free' => 'Yes',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'WordPress Login Page Protect<span>(Includes WooCommerce & XMLRPC)</span>' => [
+        'Free' => 'Yes',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Safelist/Denylist<span>(Supports IP Ranges)</span>' => [
+        'Free' => 'Yes',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Lockouts Log' => [
+        'Free' => 'Yes',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Configurable Email Notifications On Lockouts' => [
+        'Free' => 'Yes',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Trusted IP Origins' => [
+        'Free' => 'Yes',
+        'Premium' => 'Auto',
+        'Premium +' => 'Auto',
+        'Professional' => 'Auto',
+    ],
+    'Login Firewall' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Performance Optimizer - Brute Force Attacks Absorbed In The Cloud' => [
+        'Free' => 'Lock',
+        'Premium' => 'ssss',
+        'Premium +' => 'ssss',
+        'Professional' => 'ssss',
+    ],
+    'Access Active Cloud Denylist' => [
+        'Free' => 'Lock',
+        'Premium' => 'Lock',
+        'Premium +' => 'Lock',
+        'Professional' => 'Yes',
+    ],
+    'Auto IP Denylist' => [
+        'Free' => 'Lock',
+        'Premium' => 'Lock',
+        'Premium +' => 'Lock',
+        'Professional' => 'Yes',
+    ],
+    'Deny By Country' => [
+        'Free' => 'Lock',
+        'Premium' => 'Lock',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Access IP Denylist of Known Malicious IPs' => [
+        'Free' => 'Lock',
+        'Premium' => 'Lock',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'XML-RPC Denial' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Enriched IP Data' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'IP Throttling' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Synchronized IP Safelist/Denylist Between Multiple Domains' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Synchronized Lockouts Check Between Multiple Domains' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Supports IPV6 Ranges for Safelist/Denylist' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Visual Metrics' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'CSV Download of All IP Data' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Intelligent IP Denial' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Auto Backup Of All IP Data' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Enhanced Lockouts Log' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Unlock The Locked Admin' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'Email Support' => [
+        'Free' => 'Lock',
+        'Premium' => 'Yes',
+        'Premium +' => 'Yes',
+        'Professional' => 'Yes',
+    ],
+    'buttons' => [
+        'Free' => '<button class="menu__item button__transparent_orange" data-bs-toggle="modal" data-bs-target="#download_modal_window" type="button">Download</button>',
+        'Premium' => '<a href="#plans_menu_nav" class="link__style_unlink"><button class="menu__item button__orange">Subscribe</button></a>',
+        'Premium +' => '<a href="#plans_menu_nav" class="link__style_unlink"><button class="menu__item button__orange">Subscribe</button></a>',
+        'Professional' => '<a href="#plans_menu_nav" class="link__style_unlink"><button class="menu__item button__orange">Subscribe</button></a>',
+    ],
+];
+
+return $compare_list;
