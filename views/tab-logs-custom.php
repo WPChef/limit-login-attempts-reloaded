657b7c8c2f07b6e4c87974d380a180b51c1f1e90
slavadvent <slavadvent@users.noreply.github.com>
2023-11-29 16:16:28 +0300

    Line separator LF. Step-56

--- views/tab-logs-custom.php
+++ views/tab-logs-custom.php
@@ -4,8 +4,6 @@ if( !defined( 'ABSPATH' ) ) exit();
 
 ?>
 
-<!--<h3>--><?php //echo __( 'Login Firewall Settings & Logs', 'limit-login-attempts-reloaded' ); ?><!--</h3>-->
-
 <div id="llar-setting-page-logs__active" class="limit-login-app-dashboard">
 
 	<?php include_once( LLA_PLUGIN_DIR.'views/app-widgets/active-lockouts.php'); ?>
