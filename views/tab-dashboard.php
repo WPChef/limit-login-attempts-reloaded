33347d342e21a2b83297d112ba824e3e0af09ecb
slavadvent <slavadvent@users.noreply.github.com>
2024-01-08 22:36:19 +0300

    Chart-2, info-box-2. part-110

--- views/tab-dashboard.php
+++ views/tab-dashboard.php
@@ -9,7 +9,7 @@ if ( ! defined( 'ABSPATH' ) ) exit();
 
 $active_app = Config::get( 'active_app' );
 $active_app = ( $active_app === 'custom' && LimitLoginAttempts::$cloud_app ) ? 'custom' : 'local';
-
+$is_active_app_custom = $active_app === 'custom';
 
 $setup_code = Config::get( 'app_setup_code' );
 
@@ -39,10 +39,34 @@ if ($active_app === 'local' && empty($setup_code)) {
 
         <div class="info-box-2">
             <div class="section-title__new">
-                <span class="llar-label">
-                    <span class="llar-label__circle-blue">&bull;</span>
-                    <?php _e( 'Failed Login Attempts', 'limit-login-attempts-reloaded' ); ?>
-                </span>
+                <div class="llar-label-group">
+                    <span class="llar-label">
+                        <span class="llar-label__circle-blue">&bull;</span>
+                        <?php _e( 'Failed Login Attempts', 'limit-login-attempts-reloaded' ); ?>
+                        <span class="hint_tooltip-parent">
+                            <span class="dashicons dashicons-editor-help"></span>
+                            <div class="hint_tooltip">
+                                <div class="hint_tooltip-content">
+                                    <?php esc_attr_e( 'An IP that hasn\'t been previously denied by the cloud app, but has made an unsuccessful login attempt on your website.', 'limit-login-attempts-reloaded' ); ?>
+                                </div>
+                            </div>
+                        </span>
+                    </span>
+                    <?php if( $is_active_app_custom ) : ?>
+                        <span class="llar-label">
+                            <span class="llar-label__circle-grey">&bull;</span>
+                                <?php _e( 'Requests', 'limit-login-attempts-reloaded' ); ?>
+                            <span class="hint_tooltip-parent">
+                                <span class="dashicons dashicons-editor-help"></span>
+                                <div class="hint_tooltip">
+                                    <div class="hint_tooltip-content">
+                                        <?php esc_attr_e( 'A request is utilized when the cloud validates whether an IP address is allowed to attempt a login, which also includes denied logins.', 'limit-login-attempts-reloaded' ); ?>
+                                    </div>
+                                </div>
+                            </span>
+                        </span>
+	                <?php endif; ?>
+                </div>
                 <span class="llar-label__url">
                     <a class="link__style_unlink">
                         <?php echo wp_parse_url( home_url(), PHP_URL_HOST ) ?>
