6ef4a74adc4dc33cb81c1da4b65f591ef24722dd
slavadvent <slavadvent@users.noreply.github.com>
2024-02-22 15:27:52 +0300

    Fix css style dashboard page

--- views/options-page.php
+++ views/options-page.php
@@ -31,47 +31,50 @@ if ( $is_active_app_custom ) {
 	$is_agency = $block_sub_group === 'Agency';
 	$requests = ! $is_agency ? $this->info_requests() : false;
 	$is_exhausted = ! $is_agency ? $this->info_is_exhausted() : false;
+	$is_exhausted = true;
 } else {
 
 	$is_exhausted = false;
 	$block_sub_group = '';
 	$upgrade_premium_url = '';
-}
+}?>
 
+<div class="header_massage">
+    <?php
+    if ( $is_active_app_custom && $block_sub_group === 'Micro Cloud' ) :
 
-if ( $is_active_app_custom && $block_sub_group === 'Micro Cloud' ) :
+        $notifications_message_shown = Config::get( 'notifications_message_shown' );
+        $upgrade_premium_url = $this->info_upgrade_url();
 
-	$notifications_message_shown = Config::get( 'notifications_message_shown' );
-	$upgrade_premium_url = $this->info_upgrade_url();
+        if ( $is_exhausted && ! $notifications_message_shown ) : ?>
 
-    if ( $is_exhausted && ! $notifications_message_shown ) : ?>
-
-        <div id="llar-header-upgrade-premium-message" class="exhausted">
-            <p>
-                <span class="dashicons dashicons-superhero"></span>
-				<?php echo sprintf(
-					__( 'You have exhausted your monthly quota of free Micro Cloud requests. The plugin has now reverted to the free version. <a href="%s" class="link__style_color_inherit" target="_blank">Upgrade to the premium</a> version today to maintain cloud protection and advanced features.', 'limit-login-attempts-reloaded' ),
-					$upgrade_premium_url );
-				?>
-            </p>
-            <div class="close">
-                <span class="dashicons dashicons-no-alt"></span>
+            <div id="llar-header-upgrade-premium-message" class="exhausted">
+                <p>
+                    <span class="dashicons dashicons-superhero"></span>
+                    <?php echo sprintf(
+                        __( 'You have exhausted your monthly quota of free Micro Cloud requests. The plugin has now reverted to the free version. <a href="%s" class="link__style_color_inherit" target="_blank">Upgrade to the premium</a> version today to maintain cloud protection and advanced features.', 'limit-login-attempts-reloaded' ),
+                        $upgrade_premium_url );
+                    ?>
+                </p>
+                <div class="close">
+                    <span class="dashicons dashicons-no-alt"></span>
+                </div>
             </div>
-        </div>
-    <?php else : ?>
-        <div id="llar-header-upgrade-mc-message">
-            <p>
-                <span class="dashicons dashicons-superhero"></span>
-				<?php echo sprintf(
-					__( 'Enjoying Micro Cloud? To prevent interruption of the cloud app, <a href="%s" class="link__style_color_inherit" target="_blank">Upgrade to Premium</a> today', 'limit-login-attempts-reloaded' ),
-					$upgrade_premium_url );
-				?>
-            </p>
-        </div>
+        <?php else : ?>
+            <div id="llar-header-upgrade-mc-message">
+                <p>
+                    <span class="dashicons dashicons-superhero"></span>
+                    <?php echo sprintf(
+                        __( 'Enjoying Micro Cloud? To prevent interruption of the cloud app, <a href="%s" class="link__style_color_inherit" target="_blank">Upgrade to Premium</a> today', 'limit-login-attempts-reloaded' ),
+                        $upgrade_premium_url );
+                    ?>
+                </p>
+            </div>
+
+        <?php endif; ?>
 
     <?php endif; ?>
-
-<?php endif; ?>
+</div>
 
 <?php if ( ( $auto_update_choice || $auto_update_choice === null ) && !Helpers::is_auto_update_enabled() ) : ?>
 <div class="notice notice-error llar-auto-update-notice">
