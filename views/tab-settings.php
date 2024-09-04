63f6c844d3662c1db66c8505beeb6aef60984e8d
slavadvent <slavadvent@users.noreply.github.com>
2024-09-04 18:20:14 +0300

    Correction hyphenation of long (sprintf)

--- views/tab-settings.php
+++ views/tab-settings.php
@@ -259,15 +259,21 @@ $url_try_for_free_cloud     = ( $is_active_app_custom ) ? $this->info_upgrade_ur
                             <div class="item">
                                 <img class="icon" src="<?php echo LLA_PLUGIN_URL ?>assets/css/images/icon-ip-bg.png">
                                 <div class="name">
-									<?php _e( 'Use intelligent IP denial/&#8203;unblocking technology', 'limit-login-attempts-reloaded' ); ?>
+									<?php echo sprintf(
+										__( 'Use intelligent IP denial/%sunblocking technology', 'limit-login-attempts-reloaded' ),
+										'&#8203;' );
+									?>
                                 </div>
                             </div>
                             <div class="item">
                                 <img class="icon"
                                      src="<?php echo LLA_PLUGIN_URL ?>assets/css/images/icon-blocklist-bg.png">
                                 <div class="name">
-									<?php _e( 'Sync the allow/&#8203;deny/&#8203;pass lists between multiple domains', 'limit-login-attempts-reloaded' ); ?>
-                                </div>
+	                                <?php echo sprintf(
+		                                __( 'Sync the allow/%sdeny/%spass lists between multiple domains', 'limit-login-attempts-reloaded' ),
+		                                '&#8203;', '&#8203;' );
+	                                ?>
+                              </div>
                             </div>
                             <div class="item">
                                 <img class="icon"
@@ -425,13 +431,19 @@ $url_try_for_free_cloud     = ( $is_active_app_custom ) ? $this->info_upgrade_ur
                                 <div class="item">
                                     <img src="<?php echo LLA_PLUGIN_URL ?>assets/css/images/ip-min.png">
                                     <div class="name">
-										<?php _e( 'Use intelligent IP denial/&#8203;unblocking technology', 'limit-login-attempts-reloaded' ); ?>
+	                                    <?php echo sprintf(
+		                                    __( 'Use intelligent IP denial/%sunblocking technology', 'limit-login-attempts-reloaded' ),
+		                                    '&#8203;' );
+	                                    ?>
                                     </div>
                                 </div>
                                 <div class="item">
                                     <img src="<?php echo LLA_PLUGIN_URL ?>assets/css/images/cross-min.png">
                                     <div class="name">
-										<?php _e( 'Sync the allow/&#8203;deny/&#8203;pass lists between multiple domains', 'limit-login-attempts-reloaded' ); ?>
+	                                    <?php echo sprintf(
+		                                    __( 'Sync the allow/%sdeny/%spass lists between multiple domains', 'limit-login-attempts-reloaded' ),
+		                                    '&#8203;', '&#8203;' );
+	                                    ?>
                                     </div>
                                 </div>
                                 <div class="item">
