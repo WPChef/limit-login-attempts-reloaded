95734f089c18419df4b2bb814ec6d6be633d5fc6
slavadvent <slavadvent@users.noreply.github.com>
2024-11-22 16:24:22 +0300

    reset password and register new user for cloud - UM, Woo, standard WP

--- core/LimitLoginAttempts.php
+++ core/LimitLoginAttempts.php
@@ -143,6 +143,11 @@ class LimitLoginAttempts
 
 	    add_action( 'register_post', array( $this, 'register_post_hook' ), 10, 3 );
 	    add_action( 'lostpassword_post', array( $this, 'lostpassword_post_hook' ), 10, 2 );
+//		add_action( 'retrieve_password', array( $this, 'retrieve_password_hook' ), 1, 2 );
+//		add_action( 'um_submit_form_register', array( $this, 'um_register_post_hook' ), 1, 2 );
+		add_action( 'um_submit_form_errors_hook__blockedips', array( $this, 'um_submit_form_errors_hook__blockedips_hook' ), 1, 2 );
+		add_filter( 'um_custom_error_message_handler', array( $this, 'llar_um_deny_error_message' ), 10, 3 );
+		add_action( 'um_reset_password_errors_hook', array( $this, 'um_reset_password_errors_hook' ), 10, 2);
 
 		register_activation_hook( LLA_PLUGIN_FILE, array( $this, 'activation' ) );
 	}
@@ -2333,13 +2338,95 @@ class LimitLoginAttempts
 
 
 	/**
+     * Register new user standard WP, Woo
+     *
 	 * @param $user_login
 	 * @param $user_email
 	 * @param $errors
 	 *
 	 * @throws Exception
 	 */
-	public function register_post_hook( $user_login, $user_email, $errors ) {
+	public function register_post_hook( $user_login, $user_email, $errors )
+    {
+
+		if ( ! self::$cloud_app ) {
+			return;
+		}
+
+		$response = self::$cloud_app->acl_check( array(
+			'ip'        => Helpers::get_all_ips(),
+			'login'     => $user_login,
+			'gateway'   => Helpers::detect_gateway(),
+		) );
+
+	    $response['result'] = 'deny';
+
+		if ( $response['result'] === 'deny' ) {
+
+			$errors->add( 'llar_registration_disabled', __( '<strong>Error</strong>: User registration is currently not allowed.', 'limit-login-attempts-reloaded' ) );
+		}
+	}
+
+
+	/**
+	 * Register new user UM
+	 */
+	public function um_submit_form_errors_hook__blockedips_hook($args, $form_data) {
+
+	    if($form_data['mode'] === 'register' ) {
+
+		    if ( ! isset( $args['user_login'] ) ) {
+			    return;
+		    }
+
+		    $user_login = sanitize_text_field( $args['user_login'] );
+
+		    if ( ! self::$cloud_app ) {
+			    return;
+		    }
+
+		    $response = self::$cloud_app->acl_check( array(
+			    'ip'        => Helpers::get_all_ips(),
+			    'login'     => $user_login,
+			    'gateway'   => Helpers::detect_gateway(),
+		    ) );
+
+		    if ( $response['result'] === 'deny' ) {
+
+			    exit( wp_redirect( esc_url( add_query_arg( 'err', 'llar_registration_disabled' ) ) ) );
+		    }
+        }
+	}
+
+
+	/**
+     * Error for register new user UM
+     *
+	 * @param $error
+	 * @param $request_error
+	 * @param $args
+	 *
+	 * @return mixed|string|void
+	 */
+	public function llar_um_deny_error_message( $error, $request_error, $args )
+    {
+		if ( 'llar_registration_disabled' === $request_error ) {
+			$error = __( '<strong>Error</strong>: User registration is currently not allowed.', 'limit-login-attempts-reloaded' );
+		}
+		return $error;
+    }
+
+
+	/**
+	 * Reset password UM
+	 */
+    public function um_reset_password_errors_hook( $args, $form_data ) {
+
+		if ( ! isset( $args['username_b'] ) ) {
+			return;
+		}
+
+		$user_login = sanitize_text_field( $args['username_b'] );
 
 		if ( ! self::$cloud_app ) {
 			return;
@@ -2353,21 +2440,29 @@ class LimitLoginAttempts
 
 		if ( $response['result'] === 'deny' ) {
 
-			$errors->add( 'llar_registration_disabled', __( '<strong>Error</strong>: User registration is currently not allowed.', 'limit-login-attempts-reloaded' ) );
-		}
+			UM()->form()->add_error( 'username_b', __( '<strong>Error</strong>: Password recovery is currently not allowed.', 'limit-login-attempts-reloaded' ) );
+        }
 	}
 
 	/**
+     * Reset password standard WP, Woo
+     *
 	 * @param $errors
 	 * @param $user_data
 	 *
 	 * @throws Exception
 	 */
-	public function lostpassword_post_hook( $errors, $user_data ) {
+	public function lostpassword_post_hook( $errors, $user_data )
+    {
 
-		if ( ! self::$cloud_app || ! $user_data ) {
+		if ( ! self::$cloud_app ) {
 			return;
 		}
+
+	    if ( ! $user_data ) {
+		    $errors->add( 'invalidcombo', __( '<strong>Error:</strong> Password recovery is currently not allowed.' ) );
+		    return;
+	    }
 
 		$response = self::$cloud_app->acl_check( array(
 			'ip'        => Helpers::get_all_ips(),
