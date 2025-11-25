81d5f6bcbdbdd743b97c32c56eb9df75929e56df
Nikita Menshutin <nikita.menshutin@edited.com>
2025-11-25 13:11:21 +0200

    fix: set current domain in lockout notification subject

--- core/LimitLoginAttempts.php
+++ core/LimitLoginAttempts.php
@@ -1392,8 +1392,9 @@ class LimitLoginAttempts
 		$plugin_data = get_plugin_data( LLA_PLUGIN_DIR . 'limit-login-attempts-reloaded.php' );
 
 		$subject = sprintf(
-			__( "Failed login by IP %s www.limitloginattempts.com", 'limit-login-attempts-reloaded' ),
-			esc_html( $ip )
+			__( "Failed login by IP %s %s", 'limit-login-attempts-reloaded' ),
+			esc_html( $ip ),
+			esc_html( $site_domain )
 		);
 
 		ob_start();
