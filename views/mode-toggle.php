ea2583678da68945852f40ae2b1fcef3a0c17230
Nikita Menshutin <info@nikita.global>
2025-12-13 00:19:20 +0200

    chore: toggle

--- views/mode-toggle.php
+++ views/mode-toggle.php
@@ -6,20 +6,32 @@
  */
 
 if ( ! defined( 'ABSPATH' ) ) exit();
-if ( !$free_requests_exhausted ) {
-    return;
+
+if ( ! $free_requests_exhausted ) {
+	return;
 }
 ?>
 
 <div class="llar-mode-toggle-wrapper">
-    <div class="llar-mode-toggle">
-        <span class="llar-mode-toggle-item<?php echo ( $active_app === 'local' ) ? ' active' : ''; ?>">
-            <?php _e( 'Local Mode', 'limit-login-attempts-reloaded' ); ?>
-        </span>
-        <span class="llar-mode-toggle-item<?php echo ( $active_app === 'custom' ) ? ' active' : ''; ?>">
-            <?php _e( 'Cloud Mode', 'limit-login-attempts-reloaded' ); ?>
-        </span>
-    </div>
+	<div class="llar-mode-toggle">
+		<?php if ( 'local' === $active_app ) : ?>
+			<span class="llar-mode-toggle-item active">
+				<?php _e( 'Local Mode', 'limit-login-attempts-reloaded' ); ?>
+			</span>
+		<?php else : ?>
+			<a href="#" class="llar-mode-toggle-item">
+				<?php _e( 'Local Mode', 'limit-login-attempts-reloaded' ); ?>
+			</a>
+		<?php endif; ?>
+
+		<?php if ( 'custom' === $active_app ) : ?>
+			<span class="llar-mode-toggle-item active">
+				<?php _e( 'Cloud Mode', 'limit-login-attempts-reloaded' ); ?>
+			</span>
+		<?php else : ?>
+			<a href="#" class="llar-mode-toggle-item">
+				<?php _e( 'Cloud Mode', 'limit-login-attempts-reloaded' ); ?>
+			</a>
+		<?php endif; ?>
+	</div>
 </div>
-
-
