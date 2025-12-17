d51fe1aa4408b17829f219be1960591eeed7fb43
Nikita Menshutin <info@nikita.global>
2025-12-17 16:25:34 +0200

    chore: views/chart-failed-attempts.php

--- views/chart-failed-attempts.php
+++ views/chart-failed-attempts.php
@@ -9,15 +9,16 @@
  * @var array $requests
  * @var bool|string $is_exhausted
  */
+
 use LLAR\Core\Config;
+
 if ( Config::are_free_requests_exhausted() ) {
 	include __DIR__ . '/chart-failed-attempts-cloud.php';
 	include __DIR__ . '/chart-failed-attempts-local.php';
-}
-else {
-if ( $is_active_app_custom ) {
-	include __DIR__ . '/chart-failed-attempts-cloud.php';
 } else {
-	include __DIR__ . '/chart-failed-attempts-local.php';
-}
+	if ( $is_active_app_custom ) {
+		include __DIR__ . '/chart-failed-attempts-cloud.php';
+	} else {
+		include __DIR__ . '/chart-failed-attempts-local.php';
+	}
 }
